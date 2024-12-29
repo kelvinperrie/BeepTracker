42147d18eeabb4e51ca4b24f7d199d93d260e0bc
Kelvin <kelvin.perrie@tdhb.org.nz>
2024-12-30 08:51:07 +1300

    mucking around with api -> add test controller

---
+++ BeepTracker.Api/Controllers/TestController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BeepTracker.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TestController : Controller
+    {
+        [HttpGet]
+        public ActionResult<string> Get()
+        {
+            return "Hello, the site is working.";
+        }
+    }
+}
