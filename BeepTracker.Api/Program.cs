61670eb73071b88684a2e427bb61256af9251222
Kelvin <kelvin.perrie@tdhb.org.nz>
2024-12-31 09:36:53 +1300

    trying to get azure logging working

--- BeepTracker.Api/Program.cs
+++ BeepTracker.Api/Program.cs
@@ -1,6 +1,7 @@
 using BeepTracker.Api.Models;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
+using Azure.Monitor.OpenTelemetry.AspNetCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -9,6 +10,9 @@ AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 builder.Configuration.AddEnvironmentVariables();
 
 // Add services to the container.
+
+// Add OpenTelemetry and configure it to use Azure Monitor.
+builder.Services.AddOpenTelemetry().UseAzureMonitor();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -29,7 +33,7 @@ builder.Logging
 
 var app = builder.Build();
 
-
+app.Logger.LogInformation("HELLLLLLLLO");
 //var logger = (ILogger<Program>?)app.Services.GetService(typeof(ILogger<Program>));
 //logger.LogWarning("We're in the program");
 //var connectionString = builder.Configuration.GetConnectionString("BeepTrackerConnection");
