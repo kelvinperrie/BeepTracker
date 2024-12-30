0736b120e3ff4427b7cf79f3cf744f92079f1b64
Kelvin <kelvin.perrie@tdhb.org.nz>
2024-12-30 15:32:06 +1300

    more logging

--- BeepTracker.Api/Controllers/TestController.cs
+++ BeepTracker.Api/Controllers/TestController.cs
@@ -6,10 +6,18 @@ namespace BeepTracker.Api.Controllers
     [ApiController]
     public class TestController : Controller
     {
+        public readonly IConfiguration _configuration;
+
+        public TestController(ILogger<TestController> logger, IConfiguration configuration) 
+        {
+            _configuration = configuration;
+        }
+
         [HttpGet]
         public ActionResult<string> Get()
         {
-            return "Hello, the site is working?";
+            var connectionString = _configuration.GetConnectionString("BeepTrackerConnection");
+            return $"Hello, the site is working? This is the connection string {connectionString}";
         }
     }
 }
