3a748f8572963e2b856dd6f58db337221f626282
Kelvin <kelvin.perrie@tdhb.org.nz>
2025-02-10 13:50:11 +1300

    testing out some auth stuff

---
+++ BeepTracker.Common/Services/PasswordHasher.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BeepTracker.Common.Services
+{
+    // https://www.youtube.com/watch?v=J4ix8Mhi3rs
+    public class PasswordHasher
+    {
+        private const int SaltSize = 16;
+        private const int HashSize = 32;
+        private const int Iterations = 100000;
+
+        private readonly HashAlgorithmName Algorithm = HashAlgorithmName.SHA512;
+
+        public string Hash(string password)
+        {
+            byte[] salt = RandomNumberGenerator.GetBytes(SaltSize);
+            byte[] hash = Rfc2898DeriveBytes.Pbkdf2(password, salt, Iterations, Algorithm, HashSize);
+
+            return $"{Convert.ToHexString(hash)}-{Convert.ToHexString(salt)}";
+        }
+
+        public bool Verify(string password, string passwordHash)
+        {
+            string[] parts = passwordHash.Split('-');
+            byte[] hash = Convert.FromHexString(parts[0]);
+            byte[] salt = Convert.FromHexString(parts[1]);
+
+            byte[] inputHash = Rfc2898DeriveBytes.Pbkdf2(password, salt, Iterations, Algorithm, HashSize);
+
+            return CryptographicOperations.FixedTimeEquals(hash, inputHash);
+        }
+    }
+}
